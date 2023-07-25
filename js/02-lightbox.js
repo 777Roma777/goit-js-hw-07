6a22ccaff6cdc463adfc27683f96dec72ac7df9f
777Roma777 <nanoeagle911@gmail.com>
2023-07-25 14:56:35 +0300

    Task is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,12 +5,11 @@ const gallery = document.querySelector(".gallery");
 createGallery();
 
 function createGalleryElement(item) {
-  return `<li class="gallery__item">
+    return `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
       <img
         class="gallery__image"
         src="${item.preview}"
-        data-source="${item.original}"
         alt="${item.description}"
       />
     </a>
